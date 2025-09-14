867d6e08c334640dd337a062f5ed0c67dc94a02e
JoseLuisEspinolgit config --global user.name JoseLuisEspinolagit config --global user.name JoseLuisEspinol <jle36@hotmail.com>
2025-09-14 14:12:57 -0300

    Segundo commit

--- src/clase04-ejerc01/ProductList.jsx
+++ src/clase04-ejerc01/ProductList.jsx
@@ -2,9 +2,12 @@ import './ProductList.css';
 
 function ProductList({ carrito, setCarrito }) {
   const productos = [
-    { id: 1, nombre: 'Mouse', precio: 5000 },
+    { id: 1, nombre: 'Mouse', precio: 9000 },
     { id: 2, nombre: 'Teclado', precio: 35000 },
-    { id: 3, nombre: 'Monitor', precio: 45000 },
+    { id: 3, nombre: 'Monitor', precio: 180000 },
+    { id: 4, nombre: 'Tablet', precio: 110000 },
+    { id: 5, nombre: 'Impresora', precio: 255000 },
+    { id: 6, nombre: 'Pad', precio: 5000 },
   ];
 
   const agregarAlCarrito = (producto) => {
@@ -17,7 +20,12 @@ function ProductList({ carrito, setCarrito }) {
       <ul>
         {productos.map((prod) => (
           <li key={prod.id}>
-            {prod.nombre} - ${prod.precio}
+            <div>
+              {prod.nombre} 
+            </div>
+            <div>
+              ${prod.precio}
+            </div>
             <button onClick={() => agregarAlCarrito(prod)}>Agregar</button>
           </li>
         ))}
